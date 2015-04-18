d94cdafbfa3aef85682bdf096de130305409afd5
Lucien Pullen <drurowin@gmail.com>
2015-04-18 00:53:30 -0600

    basic commanding

--- client/api.lisp
+++ client/api.lisp
@@ -5,3 +5,5 @@
 (defgeneric imap4-client-host (client) (:documentation "Return the server hostname."))
 (defgeneric imap4-client-port (client) (:documentation "Return the server port number."))
 (defgeneric imap4-client-sslp (client) (:documentation "Return true when the connection uses SSL."))
+
+(defgeneric command (connection command &rest args))
