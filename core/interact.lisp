39522058a68b0a39235f28e0e8f5a0e12b7d9df3
Lucien Pullen <drurowin@gmail.com>
2015-04-18 01:39:30 -0600

    existing code to use the protocol read error condition

--- core/interact.lisp
+++ core/interact.lisp
@@ -1,27 +1,5 @@
 (cl:in-package :org.drurowin.net.imap4.1)
 
-(defun check-crlf (stream last-char char)
-  (when (and (eql last-char #\Return)
-             (not (eql char #\Linefeed)))
-    (error "In ~A, the character ~@C followed a carriage return, not a line feed." stream char)
-    #+(OR) ($simple-protocol-error stream "The character ~@C followed a carriage return, not a line feed." char)))
-
-(defun check-ascii (stream octet)
-  (when (> octet 127)
-    (error "In ~A, the code point ~X is not an ASCII character." stream octet)
-    #+(OR) (error 'ascii-error :stream stream :position (file-position stream) :octet octet)))
-
-(defmethod stream-read-line ((s fundamental-imap4-connection))
-  (with-output-to-string (out)
-    (do* ((octet (read-byte s) (read-byte s))
-          (last-char nil char)
-          (char (code-char octet) (code-char octet)))
-         ((and (eql char #\Linefeed)
-               (eq last-char #\Return)))
-      (check-ascii s octet)
-      (check-crlf s last-char char)
-      (unless (eql char #\Return) (write-char char out)))))
-
 (defmethod stream-write-string ((s fundamental-imap4-connection) o &optional start end)
   (stream-write-string (slot-value s 'stream) o start end))
 
@@ -42,8 +20,28 @@
 (defun simple-imap4-reader-error (stream control &rest args)
   (error 'simple-imap4-reader-error :stream stream :format-control control :format-arguments args))
 
+(defun check-ascii (stream octet)
+  (when (> octet 127)
+    (simple-imap4-reader-error stream "The code point ~X is not an ASCII character." octet)))
+
+(defun check-crlf (stream last-char char)
+  (when (and (eql last-char #\Return)
+             (not (eql char #\Linefeed)))
+    (simple-imap4-reader-error stream "The character ~@C followed a carriage return, not a line feed." char)))
+
 (defgeneric skip-whitespace (stream)
   (:method ((s flexi-streams:flexi-input-stream))
     (loop (if (cl-unicode:has-binary-property (code-char (flexi-streams:peek-byte s)) "White_Space")
               (read-byte s)
               (return-from skip-whitespace nil)))))
+
+(defmethod stream-read-line ((s fundamental-imap4-connection))
+  (with-output-to-string (out)
+    (do* ((octet (read-byte s) (read-byte s))
+          (last-char nil char)
+          (char (code-char octet) (code-char octet)))
+         ((and (eql char #\Linefeed)
+               (eq last-char #\Return)))
+      (check-ascii s octet)
+      (check-crlf s last-char char)
+      (unless (eql char #\Return) (write-char char out)))))
