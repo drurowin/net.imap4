96dde5678a6f7ebf6e1359326bc02c85cee3e701
Lucien Pullen <drurowin@gmail.com>
2016-08-30 15:31:33 -0600

    SEARCH command
    Here's the procedure definition.  Not quite sure on the return value
    yet, so holding off on implementation.

--- client/command.lisp
+++ client/command.lisp
@@ -5,3 +5,10 @@
 
 If the password is a symbol, the password is obtained by invoking
 `read-password' with the password as the method."))
+
+(defgeneric search (connection &rest options)
+  (:documentation "Issue a SEARCH(6.4.4) command.  On success, the search results are
+returned.
+
+If the first option is the keyword :UIDS the command returns unique
+identifiers instead of message sequence numbers."))
--- client/pkg.lisp
+++ client/pkg.lisp
@@ -5,6 +5,7 @@
 
 Client implementation.")
   (:use :cl)
+  (:shadow #:search)
   (:local-nicknames (:core :org.drurowin.net.imap4.core.1)
                     (:generic-open "//org.drurowin:2011/io@1")
                     (:mp :org.drurowin.message-processor)))
